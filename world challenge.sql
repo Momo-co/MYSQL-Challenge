36a2c7d716be0b17c949227165ccb7a2b4c1dd3a
SumanGrg <suman_grg@outlook.com>
2021-06-30 10:58:01 +0100

    Added answer to second World Challenge

--- world challenge.sql
+++ world challenge.sql
@@ -4,3 +4,8 @@ USE world;
 -- SELECT COUNT(ID), CountryCode 
 -- FROM city
 -- WHERE CountryCode = 'USA';
+
+-- world challenge 2
+-- SELECT Name, Population, LifeExpectancy
+-- FROM country
+-- WHERE Name = 'Argentina';
